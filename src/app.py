96ada3c5ae55acb70486a31c87e06f137bd361f2
Gabriel Fior <gabrielfior@Gabriels-MacBook-Pro.local>
2018-12-28 19:53:28 +0000

    Implemented Flask-Restless to easier expose db models as api endpoints

--- src/app.py
+++ src/app.py
@@ -2,9 +2,13 @@ from flask import Flask, jsonify
 
 import os
 from dotenv import load_dotenv
+from flask_restless import APIManager
+
 from src.database import db
 from flask_restful import Api
 from src.ResultResource import ResultListResource, ResultResource
+from src.models import Result
+
 
 def create_app():
 
@@ -14,17 +18,24 @@ def create_app():
     app.config['DEBUG'] = True
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
 
-    register_endpoints(app)
+    with app.app_context():
+        db.init_app(app)
+        blueprint = register_models_endpoints(app)
+        app.register_blueprint(blueprint)
+
+    #db.init_app(app)
+
 
     return app
 
-def register_endpoints(app):
-    # Flask restful
-    api = Api(app)
-    api.add_resource(ResultListResource, '/results')
-    api.add_resource(ResultResource, '/results/<result_id>')
+def register_models_endpoints(app):
+    manager = APIManager(app, flask_sqlalchemy_db=db)
+    #manager.create_api(Result, methods=['GET', 'POST', 'DELETE'])
+    blueprint = manager.create_api_blueprint(Result, methods=['GET', 'POST', 'DELETE'])
+    return blueprint
+
+
 
 def setup_database(app):
     pass
--- src/models.py
+++ src/models.py
@@ -3,6 +3,8 @@ from sqlalchemy.dialects.postgresql import JSON
 
 from src.database import db
 
+# ToDo - add Mentee, Mentor, Meeting classes
+
 class Result(db.Model):
     __tablename__ = 'results'
 
@@ -11,26 +13,5 @@ class Result(db.Model):
     result_all = db.Column(JSON)
     result_no_stop_words = db.Column(JSON)
 
-    def __init__(self, url, result_all, result_no_stop_words):
-        self.url = url
-        self.result_all = result_all
-        self.result_no_stop_words = result_no_stop_words
-
     def __repr__(self):
         return '<id {}>'.format(self.id)
-
-    @property
-    def serialize(self):
-        """Return object data in easily serializeable format"""
-        return {
-            'id': self.id,
-            'url': self.url,
-            'result_all': self.result_all,
-            'result_no_stop_words': self.result_no_stop_words
-            }
-
-def dump_datetime(value):
-    """Deserialize datetime object into string form for JSON processing."""
-    if value is None:
-        return None
-    return [value.strftime("%Y-%m-%d"), value.strftime("%H:%M:%S")]
