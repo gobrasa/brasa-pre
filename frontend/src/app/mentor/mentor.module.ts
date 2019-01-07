d394e29f717a1ab729f6af8ddbf6fac765f6d0ff
Fior <Gabriel.Fior@d-fine.com>
2019-01-07 09:13:14 +0100

    added ionic front end || refactored repo

---
+++ frontend/src/app/mentor/mentor.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { MentorPage } from './mentor.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MentorPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [MentorPage]
+})
+export class MentorPageModule {}
