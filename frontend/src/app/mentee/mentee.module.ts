8beb362f17a84838ee81f534491cb12ee61bf98e
Fior <Gabriel.Fior@d-fine.com>
2019-01-07 09:13:14 +0100

    added ionic front end || refactored repo

---
+++ frontend/src/app/mentee/mentee.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { MenteePage } from './mentee.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MenteePage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [MenteePage]
+})
+export class MenteePageModule {}
---
+++ frontend/src/app/mentee/mentee.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mentee',
+  templateUrl: './mentee.page.html',
+  styleUrls: ['./mentee.page.scss'],
+})
+export class MenteePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
