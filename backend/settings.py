7a05d03ec37bebfe9fea108b877c588ddfdba8a3
Gabriel Fior <gabrielfior@Gabriels-MacBook-Pro.local>
2019-02-05 21:42:38 +0100

    corrected app.py

--- backend/settings.py
+++ backend/settings.py
@@ -1,5 +1,7 @@
 # Flask settings
-FLASK_SERVER_NAME = 'localhost:8888'
+#FLASK_SERVER_NAME = 'localhost:8888'
+import os
+
 FLASK_DEBUG = True  # Do not use debug mode in production
 
 # Flask-Restplus settings
@@ -9,5 +11,12 @@ RESTPLUS_MASK_SWAGGER = False
 RESTPLUS_ERROR_404_HELP = False
 
 # SQLAlchemy settings
-SQLALCHEMY_DATABASE_URI = "postgresql://localhost/brasa_pre5"
+#SQLALCHEMY_DATABASE_URI = "postgresql://localhost/brasa_pre5"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+DEBUG = False
+TESTING = False
+CSRF_ENABLED = True
+SECRET_KEY = 'this-really-needs-to-be-changed'
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
