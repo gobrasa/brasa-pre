a2d4f2aa7cb7653e1d6f0c44e9c02005e90ee0c8
Gabriel Fior <gabrielfior@Gabriels-MacBook-Pro.local>
2019-03-26 00:22:45 +0100

    Updated requirements | added basic test for flask_app

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,3 +3,6 @@ from flask import url_for
 
 def test_mentees(client):
     assert client.get('/api/mentees').status_code == 200
+
+    # ToDo - follow tutorial here
+    # https://www.patricksoftwareblog.com/testing-a-flask-application-using-pytest/
