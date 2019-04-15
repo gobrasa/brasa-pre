fe272392db8d32b3e36c9e006efeb3e2d0f8e713
Gabriel Fior <gabrielfior@Gabriels-MacBook-Pro.local>
2019-04-15 09:06:54 +0200

    Updating tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,12 @@
+import requests
 from flask import url_for
+
+from backend.auth.auth import requires_auth
 
 
 def test_mentees(client):
-    assert client.get('/api/mentees').status_code == 200
+
+    assert client.get('/api/mentees').status_code == requests.codes.UNAUTHORIZED
 
     # ToDo - follow tutorial here
     # https://www.patricksoftwareblog.com/testing-a-flask-application-using-pytest/
